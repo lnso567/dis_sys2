424e9fad503ff3ae8baedb1fdc97a7b44d6fd3c3
Your Name <you@example.com>
2024-10-18 15:50:34 +0800

    更新了 README 文件

---
+++ react/components/Layout.jsx
@@ -0,0 +1,19 @@
+const Layout = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to= "/">Home</Link>
+                </li>
+                <li>
+                    <Link to= "/post">Post User</Link>
+                </li>
+                <li>
+                    <Link to= "/get">Get All User</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Layout;
